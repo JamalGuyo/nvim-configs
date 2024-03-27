971f297bd36d8a4697613b85c933c632ab6d3327
JamalGuyo <jamalguyo08@gmail.com>
2024-03-27 09:01:45 +0300

    configure markdown preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,9 +1,8 @@
 return {
-	{
-		"iamcco/markdown-preview.nvim",
-		ft = "markdown",
-		run = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	ft = { "markdown" },
+	config = function()
+		vim.fn["mkdp#util#install"]()
+	end,
 }
