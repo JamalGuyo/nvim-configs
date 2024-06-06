c062f6c1a2ce7e8be67175d3160134137e3dda3b
JamalGuyo <jamalguyo08@gmail.com>
2024-06-06 08:56:50 +0300

    new theme and some plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,14 +9,15 @@ return {
 	-- 		}
 	-- 	end,
 	-- },
-	{
-		"sainnhe/sonokai",
-		priority = 1000,
-		config = function()
-			vim.g.sonokai_transparent_background = "1"
-			vim.g.sonokai_enable_italic = "1"
-			vim.g.sonokai_style = "andromeda"
-			vim.cmd.colorscheme("sonokai")
-		end,
-	},
+	-- {
+	-- 	"sainnhe/sonokai",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.g.sonokai_transparent_background = "1"
+	-- 		vim.g.sonokai_enable_italic = "1"
+	-- 		vim.g.sonokai_style = "andromeda"
+	-- 		vim.cmd.colorscheme("sonokai")
+	-- 	end,
+	-- },
+	{ "rose-pine/neovim", name = "rose-pine" },
 }
